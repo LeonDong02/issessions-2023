77166a87ea008c32ab1736c77367cfe56d40d8b2
Leon Dong <leon.d1043@gmail.com>
2023-01-15 17:22:30 -0500

    timefiles writeup

---
+++ Time Files/fixpng.py
@@ -0,0 +1,24 @@
+w = []
+temp = []
+image = open("flag.png", "rb")
+
+while 1:
+    cur = image.read(1)
+    if not cur:
+        break
+    temp.append(cur)
+    if len(temp) == 4:
+        while len(temp):
+            w.append(temp.pop())
+
+image.close()
+
+while len(temp):
+    w.append(temp.pop())
+
+while temp:
+    w.append(temp.pop())
+
+image = open("fixed.png", "wb")
+for i in w:
+    image.write(i)
